db3191ecb0fc6621bf558b8b4615f1f71eb7e6fb
Morgane FRISBY <mfrisby@e1r6p4.42.fr>
2018-02-26 17:28:56 +0100

    UDP remove malloc fakecam

--- srcs/raytracelight.c
+++ srcs/raytracelight.c
@@ -15,47 +15,56 @@ t_ray *get_intersection(t_cam *cam, t_ray *ray, float d)
     return (intersection);
 }
 
-static int     get_light_colision(t_ray *i, t_ray *r, t_data *data)
+static int     get_light_colision_cone_plan(t_ray *i, t_ray *r, t_data *data)
 {
-    t_cam *fakecam;
+    t_cam fakecam;
     t_cone *cone;
-    t_sphere *sphere;
-    t_cylindre *cylindre;
     t_plan *plan;
 
-    fakecam = malloc(sizeof(t_cam));
-    fakecam->camx = i->x;
-    fakecam->camy = i->y;
-    fakecam->camz = i->z;
+    fakecam.camx = i->x;
+    fakecam.camy = i->y;
+    fakecam.camz = i->z;
     cone = data->cone_head;
     while (cone)
     {
-        if (cone != data->current && calcul_cone(fakecam,r,cone) > 0)
+        if (cone != data->current && calcul_cone(&fakecam, r, cone) > 0)
             return(-1);
         cone = cone->next;
     }
+    plan = data->plan_head;
+    while (plan)
+    {
+        if (plan != data->current && calcul_plan(&fakecam,r,plan) > 0)
+            return(-1);
+        plan = plan->next;
+    }
+    return (0);
+}
+
+static int     get_light_colision_cy_sphere(t_ray *i, t_ray *r, t_data *data)
+{
+    t_cam fakecam;
+    t_sphere *sphere;
+    t_cylindre *cylindre;
+
+    fakecam.camx = i->x;
+    fakecam.camy = i->y;
+    fakecam.camz = i->z;
     sphere = data->sphere_head;
     while (sphere)
     {
-        if (sphere != data->current && calcul_sphere(fakecam,r,sphere) > 0)
+        if (sphere != data->current && calcul_sphere(&fakecam, r, sphere) > 0)
             return(-1);
         sphere = sphere->next;
     }
     cylindre = data->cylindre_head;
     while (cylindre)
     {
-        if (cylindre != data->current && calcul_cylindre(fakecam,r,cylindre) > 0)
+        if (cylindre != data->current && calcul_cylindre(&fakecam,r,cylindre) > 0)
             return(-1);
         cylindre = cylindre->next;
     }
-    plan = data->plan_head;
-    while (plan)
-    {
-        if (plan != data->current && calcul_plan(fakecam,r,plan) > 0)
-            return(-1);
-        plan = plan->next;
-    }
-     return (0);
+    return (0);
 }
 
 int  get_light_at(float *xyz, int *color, t_ray *intersection, t_light *light, t_data *data)
@@ -79,9 +88,9 @@ int  get_light_at(float *xyz, int *color, t_ray *intersection, t_light *light, t
     r->y = light->y - intersection->y;
     r->z = light->z - intersection->z;
     r = normalize(r->x, r->y, r->z);
-    if ((get_light_colision(intersection, r, data)) == -1)
+    if ((get_light_colision_cy_sphere(intersection, r, data)) == -1
+        || (get_light_colision_cone_plan(intersection, r, data)) == -1)
         return (get_color(color[0], color[1], color[2], 0.8f, 0));//ombre lumiere ambiance
     dot = get_dot(p->x, p->y, p->z, r->x, r->y, r->z);
     return (get_color(color[0], color[1], color[2], 0.8f, dot));
 }
-
