e922758f6f48c53606391ea976315a2306b2d81c
Morgane FRISBY <mfrisby@e1r6p4.42.fr>
2018-02-28 15:38:15 +0100

    UPD remove unused var

--- srcs/raytracer.c
+++ srcs/raytracer.c
@@ -18,13 +18,11 @@ static void raytrace(t_ray *ray, t_data *data, int x, int y)
 {
     int color;
     float max_d;
-    float d;
     float xyz[3];
     int *rgb;
 
     color = 0;
     max_d = 3.4028234664e+38;
-    d = 0;
     max_d = while_sphere(data, ray, xyz, &rgb);
     max_d = while_plan(data, ray, xyz, &rgb, max_d);
     max_d = while_cylindre(data, ray, xyz, &rgb, max_d);
@@ -50,7 +48,6 @@ void rayloop(t_data *data, t_upleft *upleft, t_pix *pix)
         x = 0;
         while (x < WIDTH)
         {
-
 		    mlx_hook(data->win, 3, 3, key_hook, data);
             ray = init_ray(x, y, upleft, data->cam, pix);
             raytrace(ray, data, x, y);
