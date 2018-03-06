81bbd66fd487ca87161a34be21fd7fae59b040aa
Morgane FRISBY <mfrisby@e1r7p7.42.fr>
2018-03-06 16:01:00 +0100

    MOVE colision to get_colision.c

---
+++ srcs/get_colision.c
@@ -0,0 +1,88 @@
+#include "../inc/rtv1.h"
+
+static int colision_cone(t_ray *r, t_data *data)
+{
+    float d;
+    t_cam fakecam;
+    t_cone *cone;
+
+    fakecam.camx = data->intersection->x;
+    fakecam.camy = data->intersection->y;
+    fakecam.camz = data->intersection->z;
+    cone = data->cone_head;
+    while (cone)
+    {
+        d = calcul_cone(&fakecam, r, cone);
+        if (cone != data->current && d > 0 && d < data->max_d)
+            return(-1);
+        cone = cone->next;
+    }
+    return (0);
+}
+
+static int colision_sphere(t_ray *r, t_data *data)
+{
+    float d;
+    t_cam fakecam;
+    t_sphere *sphere;
+
+    fakecam.camx = data->intersection->x;
+    fakecam.camy = data->intersection->y;
+    fakecam.camz = data->intersection->z;
+    sphere = data->sphere_head;
+    while (sphere)
+    {
+        d = calcul_sphere(&fakecam, r, sphere);
+        if (sphere != data->current && d > 0 && d < data->max_d)
+            return(-1);
+        sphere = sphere->next;
+    }
+    return (0);
+}
+
+static int colision_cylindre(t_ray *r, t_data *data)
+{
+    float d;
+    t_cam fakecam;
+    t_cylindre *cylindre;
+
+    fakecam.camx = data->intersection->x;
+    fakecam.camy = data->intersection->y;
+    fakecam.camz = data->intersection->z;
+    cylindre = data->cylindre_head;
+    while (cylindre)
+    {
+        d = calcul_cylindre(&fakecam, r, cylindre);
+        if (cylindre != data->current && d > 0 && d < data->max_d)
+            return(-1);
+        cylindre = cylindre->next;
+    }
+    return (0);
+}
+
+static int colision_plan(t_ray *r, t_data *data)
+{
+    float d;
+    t_cam fakecam;
+    t_plan *plan;
+    fakecam.camx = data->intersection->x;
+    fakecam.camy = data->intersection->y;
+    fakecam.camz = data->intersection->z;
+    plan = data->plan_head;
+    while (plan)
+    {
+        d = calcul_plan(&fakecam, r, plan);
+        if (plan != data->current && d > 0 && d < data->max_d)
+            return(-1);
+        plan = plan->next;
+    }
+    return (0);
+}
+
+int         get_colision(t_ray *r, t_data *data)
+{
+    if ((colision_cone(r, data)) == -1 || (colision_cylindre(r, data)) == -1
+        || (colision_plan(r, data)) == -1 || (colision_sphere(r, data)) == -1)
+        return (-1);
+    return (0);
+}
--- srcs/raytracelight.c
+++ srcs/raytracelight.c
@@ -1,70 +1,11 @@
 #include "../inc/rtv1.h"
 
-// static int     get_light_colision_cone_plan(t_ray *r, t_data *data)
-// {
-//     t_cam fakecam;
-//     t_cone *cone;
-//     t_plan *plan;
-
-//     fakecam.camx = data->intersection->x;
-//     fakecam.camy = data->intersection->y;
-//     fakecam.camz = data->intersection->z;
-//     cone = data->cone_head;
-//     while (cone)
-//     {
-//         if (cone != data->current && (calcul_cone(&fakecam, r, cone)) > 0)
-//             return(-1);
-//         cone = cone->next;
-//     }
-//     plan = data->plan_head;
-//     while (plan)
-//     {
-//         if (plan != data->current && (calcul_plan(&fakecam,r,plan)) > 0)
-//             return(-1);
-//         plan = plan->next;
-//     }
-//     return (0);
-// }
-
-// static int     get_light_colision_cy_sphere(t_ray *r, t_data *data)
-// {
-//     t_cam fakecam;
-//     t_sphere *sphere;
-//     t_cylindre *cylindre;
-
-//     fakecam.camx = data->intersection->x;
-//     fakecam.camy = data->intersection->y;
-//     fakecam.camz = data->intersection->z;
-//     sphere = data->sphere_head;
-//     while (sphere)
-//     {
-//         if (sphere != data->current && (calcul_sphere(&fakecam, r, sphere)) > 0)
-//             return(-1);
-//         sphere = sphere->next;
-//     }
-//     cylindre = data->cylindre_head;
-//     while (cylindre)
-//     {
-//         if (cylindre != data->current && (calcul_cylindre(&fakecam,r,cylindre)) > 0)
-//             return(-1);
-//         cylindre = cylindre->next;
-//     }
-//     return (0);
-// }
-
-static float  get_dot(t_light *l, t_data *data)
+static void init_ray_light(t_ray *r, t_data *data, t_light *l)
 {
-    t_ray *r;
-    r = malloc(sizeof(t_ray));
-    // if ((get_light_colision_cy_sphere(r, data)) == -1
-    //     || (get_light_colision_cone_plan(r, data)) == -1)
-    //         return (-1);
-    r->x = l->x - data->intersection->x;
-    r->y = l->y - data->intersection->y;
-    r->z = l->z - data->intersection->z;
-    r = normalize(r);
-    data->object_vect = normalize(data->object_vect);
-    return ((data->object_vect->x * r->x) + (data->object_vect->y * r->y) + (data->object_vect->z * r->z));
+        r->x = l->x - data->intersection->x;
+        r->y = l->y - data->intersection->y;
+        r->z = l->z - data->intersection->z;
+        r = normalize(r);
 }
 
 int while_light(int *rgb, t_data *data)
@@ -72,15 +13,21 @@ int while_light(int *rgb, t_data *data)
     float dot;
     int *color;
     t_light *l;
+    t_ray *r;
 
+    r = malloc(sizeof(t_ray));
     color = malloc(sizeof(int) * 3);
     color[0] = 0;
     color[1] = 0;
     color[2] = 0;
     l = data->light_head;
+    data->object_norme = normalize(data->object_norme);
     while (l)
     {
-        dot = get_dot(l, data);
+        init_ray_light(r, data, l);
+        if (get_colision(r, data) < 0)
+            return (-1);
+        dot = (data->object_norme->x * r->x) + (data->object_norme->y * r->y) + (data->object_norme->z * r->z);
         color = get_color(color, rgb, l, dot);
         l = l->next;
     }
