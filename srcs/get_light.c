58a809fa8db7dab312331d60859490c149979686
Morgane FRISBY <mfrisby@e1r4p4.42.fr>
2018-03-07 10:03:01 +0100

    FIX parse light

--- srcs/get_light.c
+++ srcs/get_light.c
@@ -6,7 +6,7 @@
 /*   By: mfrisby <mfrisby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/22 17:03:41 by mfrisby           #+#    #+#             */
-/*   Updated: 2018/03/06 17:40:58 by mfrisby          ###   ########.fr       */
+/*   Updated: 2018/03/07 10:02:00 by mfrisby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,13 +60,10 @@ static int	get_attribu(char **tab, int i, t_light *light)
 	{
 		light->coef = ft_getfloat(tab2[1]);
 		if (light->coef < 0.1f)
-			ft_putendl("\033[0;33mWarning: A spot has light
-					coef less than 0.1f.\033[0m");
+			ft_putendl("\033[0;33mWarning: A spot has light coef less than 0.1f.\033[0m");
 	}
 	else if (ft_strcmp(tab2[0], "color") == 0
 		&& get_col(tab2[1], light) == -1)
-		return (-1);
-	else
 		return (-1);
 	free(tab2[0]);
 	free(tab2[1]);
